fe21833dfa7d02a5438a3c6cd867dcbb06f91aec
Arnaud BODEL <contact@osw3.net>
2023-03-02 12:33:05 +0100

    -

--- myLibrary/src/app/modules/book/book.module.ts
+++ myLibrary/src/app/modules/book/book.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookRoutingModule } from './book-routing.module';
-import { IndexComponent } from './index/index.component';
-import { DetailComponent } from './detail/detail.component';
-import { CreateComponent } from './create/create.component';
-import { UpdateComponent } from './update/update.component';
-import { DeleteComponent } from './delete/delete.component';
+import { IndexComponent } from './components/index/index.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
 @NgModule({
   declarations: [
